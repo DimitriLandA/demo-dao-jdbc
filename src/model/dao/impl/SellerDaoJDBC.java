0cdb2b1120eb886432f75d3fcbb1f7acb6267075
Dimitri <dimitri@drogariaspovao.com.br>
2025-02-18 16:53:26 -0300

    findById Implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,13 +4,28 @@
  */
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import java.util.List;
 import model.dao.SellerDao;
 import model.entities.Seller;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import model.entities.Department;
 
 public class SellerDaoJDBC implements SellerDao {
-
+    
+    private Connection conn;
+    
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+    
+    public SellerDaoJDBC() {
+    }
+    
     @Override
     public void insert(Seller obj) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -23,12 +38,46 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+      
+      
     }
 
     @Override
     public Seller findById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+      ResultSet rs = null;
+      
+      try{
+          st = conn.prepareStatement("SELECT seller.*, department.Name as DepName" +
+                                     "FROM seller INNER JOIN department " +
+                                     "ON seller.DepartmentId = department.Id "  +
+                                     "WHERE seller.Id = ?");
+          
+          st.setInt(1, id);
+          rs = st.executeQuery();
+          
+          if(rs.next()){
+              Department dep = new Department();
+              dep.setId(rs.getInt("Department"));
+              dep.setName(rs.getString("DepName"));
+              
+              Seller obj = new Seller();
+              obj.setId(rs.getInt("id"));
+              obj.setName(rs.getString("Name"));
+              obj.setEmail(rs.getString("Email"));
+              obj.setBaseSalary(rs.getDouble("BaseSalary"));
+              obj.setBirthDate(rs.getDate("BirthDate"));
+              obj.setDepartment(dep);
+              return obj;
+          }
+          return null;
+      }catch(SQLException e){
+          throw new DbException(e.getMessage());
+      } finally{
+          DB.closeStatement(st);
+          DB.closeResultSet(rs);
+      }
+      
     }
 
     @Override
