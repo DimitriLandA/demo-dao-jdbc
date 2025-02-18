40362fc7bad6ae422e6737dd0009c684a30f2c2e
Dimitri <dimitri@drogariaspovao.com.br>
2025-02-18 15:40:17 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -4,10 +4,22 @@
  */
 package model.dao;
 
+import java.util.List;
+import model.entities.Department;
+
 /**
  *
  * @author dimitri_povao
  */
 public interface DepartmentDao {
-    
+
+    void insert(Department obj);
+
+    void update(Department obj);
+
+    void deleteById(Integer id);
+
+    Department findById(Integer id);
+
+    List<Department> findAll();
 }
